c3b3057727719ddc6187deb2db14c2267f7f0c28
Tsimur Hadeliya <tgadeliya@gmail.com>
2025-10-06 12:37:59 +0300

    Finish init version of running from config

--- src/matcha/run.py
+++ src/matcha/run.py
@@ -1,54 +1,27 @@
 import argparse
-from dataclasses import fields, is_dataclass
+from collections.abc import Callable
 from pathlib import Path
-from typing import Any, Union, get_args, get_origin
 
 from matcha.configs import TokenizerTrainConfig, TrainerConfig
-from matcha.tokenizers.bpe import BPETrainer
+from matcha.tokenizers import BPETrainer
 from matcha.trainer import Trainer
-
-TASKS = {
-    "train": (TrainerConfig, Trainer),
-    "train_tokenizer": (TokenizerTrainConfig, BPETrainer),
-}
 
 
 def train_tokenizer(cfg: TokenizerTrainConfig):
-    bpe_trainer = BPETrainer(
-        corpus_path=Path("data/TinyStoriesV2-GPT4-valid.txt"),
-        vocab_size=10_000,
-        special_tokens=["<|endoftext|>"],
-    )
-
+    bpe_trainer = BPETrainer(cfg)
     bpe_trainer.train()
     bpe_trainer.save_tokenizer()
 
 
-def train_language_model():
-    cfg = TrainerConfig(
-        vocab_size=20000,
-        max_steps=10,
-        val_steps=5,
-        val_every_steps=5,
-        batch_size=16,
-        context_length=512,
-        d_model=256,
-        device="cuda:0",
-        num_layers=3,
-        num_heads=8,
-        d_ff=512,
-        theta=10000,
-        lr=1e-4,
-        weight_decay=1e-1,
-        betas=(0.9, 0.95),
-        eps=1e-8,
-        checkpoint_dir="data/checkpoints",
-        train_data_path="/Users/tsimur.hadeliya/code/language_modeling/data/corpus_tokenized/tiny_stories_validation.npy",
-        val_data_path="/Users/tsimur.hadeliya/code/language_modeling/data/corpus_tokenized/tiny_stories_validation.npy",
-    )
-
+def train_language_model(cfg: TrainerConfig):
     trainer = Trainer(cfg)
     trainer.train()
+
+
+TASKS: dict[str, tuple[type, Callable]] = {
+    "train": (TrainerConfig, train_language_model),
+    "train_tokenizer": (TokenizerTrainConfig, train_tokenizer),
+}
 
 
 def main(argv: list[str] | None = None) -> None:
@@ -68,9 +41,9 @@ def main(argv: list[str] | None = None) -> None:
         help="Path to the TOML config file.",
     )
     args = parser.parse_args(argv)
-    config_cls, task_runner = TASKS[args.task][0]
-    config = config_cls.from_file(Path(args.config_path))
-    task_runner(config).run()
+    config_cls, task_runner = TASKS[args.task]
+    cfg = config_cls.from_file(Path(args.config_path))
+    task_runner(cfg)
 
 
 if __name__ == "__main__":
